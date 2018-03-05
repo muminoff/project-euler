b02d40f42a1d06c5ddb8e991a39eae8bbe3530c5
Sardor Muminov <sardor@insilicogen.com>
2018-03-05 14:00:59 +0900

    Add summation of primes

---
+++ euler_summation_of_primes/src/main.rs
@@ -0,0 +1,37 @@
+const MAX: u64 = 2_000_000;
+fn main() {
+    println!("Generating prime numbers upto 2 million...");
+    let numbers: Vec<u64> = generate_prime_numbers(MAX);
+    let sum: u64 = numbers.into_iter().sum();
+    println!("{}", sum);
+}
+
+fn generate_prime_numbers(max: u64) -> Vec<u64> {
+    let mut numbers: Vec<u64> = Vec::new();
+    for x in 1..max + 1 {
+        if is_prime(x) {
+            numbers.push(x);
+        }
+    }
+    numbers
+}
+
+fn is_prime(num: u64) -> bool {
+    if num < 2 {
+        return false;
+    }
+
+    if num % 2 == 0 {
+        return num == 2;
+    }
+
+    let mut k = 3;
+    while k * k <= num {
+        if num % k == 0 {
+            return false;
+        }
+        k += 2;
+    }
+
+    true
+}
---
+++ euler_summation_of_primes/target/debug/euler_summation_of_primes.d
@@ -0,0 +1,1 @@
+/Users/sardor/project-euler/euler_summation_of_primes/target/debug/euler_summation_of_primes.dSYM: /Users/sardor/project-euler/euler_summation_of_primes/src/main.rs
