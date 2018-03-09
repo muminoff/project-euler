248c35f1c0b37df333130701e40da568ce415fdd
Sardor Muminov <sardor@insilicogen.com>
2018-03-09 11:04:56 +0900

    Cleanup

--- euler_pythagorean_triplet/target/debug/deps/euler_pythagorean_triplet-962902322dcf8a16.d
+++
@@ -1,5 +0,0 @@
-/Users/sardor/project-euler/euler_pythagorean_triplet/target/debug/deps/euler_pythagorean_triplet-962902322dcf8a16: src/main.rs
-
-/Users/sardor/project-euler/euler_pythagorean_triplet/target/debug/deps/euler_pythagorean_triplet-962902322dcf8a16.d: src/main.rs
-
-src/main.rs:
--- euler_pythagorean_triplet/target/release/euler_pythagorean_triplet.d
+++
@@ -1,1 +0,0 @@
-/Users/sardor/project-euler/euler_pythagorean_triplet/target/release/euler_pythagorean_triplet.dSYM: /Users/sardor/project-euler/euler_pythagorean_triplet/src/main.rs
