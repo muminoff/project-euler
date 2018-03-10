d15b29e8704991fa3b48f67b1cfad508d5f4bc74
Sardor Muminov <sardor@insilicogen.com>
2018-03-10 22:57:59 +0900

    Tested and finished longest collatz sequence

--- euler_longest_collatz_sequence/src/main.rs
+++ euler_longest_collatz_sequence/src/main.rs
@@ -1,10 +1,13 @@
+use std::collections::BTreeMap;
+
 enum Number {
     Even,
     Odd,
 }
 
 fn main() {
-    for x in 1..1_000_000 {
+    let mut collatz_map = BTreeMap::new();
+    for x in 0..1_000_001 {
         let mut num: u64 = x;
         let mut numbers: Vec<u64> = vec![];
         numbers.push(num);
@@ -17,8 +20,9 @@ fn main() {
                 }
             }
         }
-        println!("{} => {}", x, numbers.len());
+        collatz_map.insert(numbers.len(), x);
     }
+    println!("{:?}", collatz_map);
 }
 
 fn check_number(n: u64) -> Number {
@@ -38,3 +42,11 @@ fn process_odd(num: &u64) -> u64 {
     let x = 3 * num + 1;
     x
 }
+
+// fn main() {
+//     let mut map = BTreeMap::new();
+//     map.insert(10, 2);
+//     map.insert(1, 10);
+//     map.insert(0, 214124);
+//     println!("{:?}", map);
+// }
